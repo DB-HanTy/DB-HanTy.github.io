8dc62fbcaa432992bfe70bd70dd70c7b60b80d81
Hty <15161656368@163.com>
2025-07-15 01:10:35 +0800

    Site updated: 2025-07-15 01:10:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/dfb8a81f.html","posts/4a17b156.html","posts/7a6a6b5a.html","posts/aeb0acd.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/4a17b156.html","posts/dfb8a81f.html","posts/aeb0acd.html","posts/7a6a6b5a.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
