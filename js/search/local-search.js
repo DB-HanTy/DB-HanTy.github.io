4bd00cc8cd0c2a7b4ea7e4187c4abd039c266128
Hty <15161656368@163.com>
2025-07-15 12:20:01 +0800

    Site updated: 2025-07-15 12:20:00

--- js/search/local-search.js
+++ js/search/local-search.js
@@ -330,8 +330,7 @@ window.addEventListener('load', () => {
   }
 
   const searchClickFn = () => {
-    btf.addEventListenerPjax(document.querySelector('#search-button > .search'), 'click', openSearch)
-    document.querySelector('#menu-search').addEventListener('click', openSearch)  
+    btf.addEventListenerPjax(document.querySelector('#search-button > .search'), 'click', openSearch)  
   }
 
   const searchFnOnce = () => {
